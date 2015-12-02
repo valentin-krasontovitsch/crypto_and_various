97ca06a172e3dc847da540f5e2565e9fb2897c31
valentin-krasontovitsch <v.krasontov@gmail.com>
2015-12-02 12:56:03 +0100

    start video file hasing assignment

---
+++ video_hash.py
@@ -0,0 +1,12 @@
+### NOTES NOTES NOTES
+### work with options passed from cmdline
+### need check function for given hash and file (??)
+### need function computing hash
+### do we want to create a hashed file?
+### END NOTES END NOTES END NOTES
+### include AES, Random from crypto lib
+
+from Crypto.Hash import SHA256
+import argparse
+
+
