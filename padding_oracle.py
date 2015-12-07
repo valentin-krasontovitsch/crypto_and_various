6080f201b034f908c70c45d090d0d427d29e4e5f
valentin-krasontovitsch <v.krasontov@gmail.com>
2015-12-07 23:24:56 +0100

    convert code using 2to3

--- padding_oracle.py
+++ padding_oracle.py
@@ -1,4 +1,4 @@
-import urllib2
+import urllib.request, urllib.error, urllib.parse
 import sys
 
 TARGET = 'http://crypto-class.appspot.com/po?er='
@@ -7,18 +7,27 @@ TARGET = 'http://crypto-class.appspot.com/po?er='
 #--------------------------------------------------------------
 class PaddingOracle(object):
     def query(self, q):
-        target = TARGET + urllib2.quote(q)    # Create query URL
-        req = urllib2.Request(target)         # Send HTTP request to server
+        target = TARGET + urllib.parse.quote(q)    # Create query URL
+        req = urllib.request.Request(target)         # Send HTTP request to server
         try:
-            f = urllib2.urlopen(req)          # Wait for response
-        except urllib2.HTTPError, e:          
-            print "We got: %d" % e.code       # Print response code
+            f = urllib.request.urlopen(req)          # Wait for response
+        except urllib.error.HTTPError as e:          
+            print("We got: %d" % e.code)       # Print response code
             if e.code == 404:
                 return True # good padding
             return False # bad padding
 
 if __name__ == "__main__":
     po = PaddingOracle()
-    po.query(sys.argv[1])       # Issue HTTP query with the given argument
+    #po.query(sys.argv[1])       # Issue HTTP query with the given argument
+    
     orig = "f20bdba6ff29eed7b046d1df9fb7000058b1ffb4210a580f748b4ac714c001bd4a61044426fb515dad3f21f18aa577c0bdf302936266926ff37dbf7035d5eeb4"
-    print(len(orig))
+    
+    #print(len(orig))
+    
+    guess = "1" # this will
+    
+    
+    po.query(orig)       # Issue HTTP query with the given argument
+    
+    
